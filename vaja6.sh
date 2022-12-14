0298241e770ca52cad8a99cd00317378b1ee8609
tejca123 <120402908+tejca123@users.noreply.github.com>
2022-12-14 21:10:09 +0100

    Update vaja6.sh

--- vaja6.sh
+++ vaja6.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-
-sudo mkdir Downloads
-sudo mkdir Desktop
-sudo mkdir Documents
-sudo mkdir Pictures
-sudo mkdir Videos
-
-for i in {1..5}
+mkdir Desktop Documents Downloads Music Pictures Public Templates Videos
+for i in 1 2 3 4 5
 do
-mkdir folder$i
+    mkdir folder_$i
 done
-
-readarray -t a <users.txt
-
-for i in ${1..5}
+for i in 1 2 3 4 5
 do
-sudo adduser $i
-sudo usermod -aG sudo $i
+    sudo adduser user_$i
+    sudo adduser user_$i sudo
 done
-
-
-sudo apt-get update -y
-sudo apt-get install -y net-tools
-
-curl -fsSL https://get.docker.com/ -o get-docker.sh
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get install ufw
+sudo ufw enable
+systemctl status ufw
+sudo apt-get update
+sudo apt-get install git
+dpkg -s git
+sudo apt-get update
+sudo apt-get install net-tools
+dpkg -s net-tools
+sudo apt-get update
+sudo apt-get install nginx
+systemctl status nginx
+curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sh install.sh
 make shellcheck
