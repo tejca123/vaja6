f42b6e50f0aff22448c1fc837e6d5f4549c27ae8
tejca123 <120402908+tejca123@users.noreply.github.com>
2022-12-14 14:58:25 +0100

    Update vaja6.sh

--- vaja6.sh
+++ vaja6.sh
@@ -19,15 +19,6 @@ sudo adduser "$i"
 sudo usermod -aG sudo "$i"
 done
 
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-    
-    sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 sudo apt-get update -y
 sudo apt-get install -y net-tools
